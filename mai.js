d29735387141f359df7eec5a744fa1ea0f4f5dc4
OlenaSorokina <114348764+elenka-lev@users.noreply.github.com>
2025-09-17 21:29:34 +0300

    cards rotate

---
+++ mai.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.container'); //отримуємо контейнер
+
+
+fetch('/cards.json') //шлях до списку карток, отримуємо проміс
+    .then(response => response.json()) //преобразуємо формат json у JS
+    .then(cards => {
+        container.innerHTML = cards.map(card => {
+            return `
+        <div class="card">
+            <div class="card-front"></div>
+            <div class="card-back" id='${card.id}' >
+                
+                    <img class="svg" src="${card.rang}" alt="some card"/>
+                
+            </div>
+        </div>`;
+        }).join(''); //додаємо до html
+    })
+    .catch(err => console.log(err)); //відловлюєио помилки
